db7dac8317bf06edd7bd0e8ffd7d777cb4d41f02
Sebas-ctrl <210505@utxicotepec.edu.mx>
2022-08-20 18:11:30 -0500

    Cambios del Blog Comentarios

--- blog.js
+++ blog.js
@@ -1,7 +1,7 @@
-axios.get('http://localhost:3000/blog')
+/* axios.get('http://localhost:3000/blog')
   .then(function (response) {
     // handle success
-    mostrarProductos(response.data)
+    mostrarMensaje(response.data)
   })
   .catch(function (error) {
     // handle error
@@ -10,43 +10,47 @@ axios.get('http://localhost:3000/blog')
   .then(function () {
     // always executed
   });
-
+ */
 
   document.getElementById('form').addEventListener('submit',function(event){
     event.preventDefault()
    
   })
-    
-   
 
    function guardarMensaje(){
     let mensaje = document.getElementById('mensaje').value
 
-    axios.post('http://localhost:3000/blog',mensaje)
+    axios.post('http://localhost:3000/blog', mensaje).then(function (response) {
+      // handle success
+      mostrarMensaje(response.data.comentario)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
    }
 
-
-   
-function mostrarMensaje(){
+function mostrarMensaje(mensaje){
+  
     let contenedorDiv = document.getElementById('comentarios')
     
-        let mensaje = document.createElement('div')
-        mensaje.classList.add("comments")
+        let comentario = document.createElement('div')
+        comentario.classList.add("single-comment")
   
-        mensaje.innerHTML = `
+        comentario.innerHTML = `
         <div class="single-comment">
         <img src="https://via.placeholder.com/80x80" alt="#">
         <div class="content">
-            <h4>Alisa harm <span>At 8:59 pm On Feb 28, 2018</span></h4>
-            <p>Enthusiastically leverage existing premium quality vectors with enterprise-wide innovation collaboration Phosfluorescently leverage others enterprisee  Phosfluorescently leverage.</p>
+            <h4>Usuario x <span>At 8:59 pm On Feb 28, 2018</span></h4>
+            <p>${mensaje}</p>
             <div class="button">
-                <a href="#" class="btn"><i class="fa fa-reply" aria-hidden="true"></i>Reply</a>
+                <a href="#" class="btn"><i class="fa fa-reply" aria-hidden="true"></i>Respuesta</a>
             </div>
+          </div>
         </div>
-    </div>
         `
         
-        contenedorDiv.appendChild(mensaje)
+        contenedorDiv.appendChild(comentario)
     }
   
    
